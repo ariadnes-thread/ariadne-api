624222e886c757650dcadf24081b6d827480f51d
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2018-04-11 23:47:25 -0700

    Add DB support, points of interest endpoint

--- doc/api_project.js
+++ doc/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-04-11T04:16:58.774Z",
+    "time": "2018-04-12T06:44:04.163Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
